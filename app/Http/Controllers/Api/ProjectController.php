806eac04cf7876abfa9f63ee7ed917a1f74eaebe
DylanBlanco <dylanblanco1198@gmail.com>
2024-11-10 19:41:54 +0100

    creato api/projectController

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//Models
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        $projects = Project::paginate(3);
+        return response()->json([
+            'image' => $projects['image'],
+            'title' => $projects['title'],
+            'author' => $projects['author'],
+            'date_create' => $projects['date_create'],
+            'description' => $projects['description'],
+        ]);
+    }
+}
